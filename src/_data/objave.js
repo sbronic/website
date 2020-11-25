46419faee93c0338958f0e5cfcb3c62590e41768
Siniša Perović <sinisaperovic@webshaped.biz>
2020-11-25 11:13:13 +0100

    GraphCMS connection

---
+++ src/_data/objave.js
@@ -0,0 +1,78 @@
+// required packages
+const fetch = require("node-fetch");
+
+// get Objave
+async function getObjave() {
+    
+    // Objave array
+    let sveobjave = [];
+
+    try {
+        // initiate fetch
+        const graphcms = await fetch("https://api-eu-central-1.graphcms.com/v2/ckhhcs4a0ds0501xs7lx6803e/master", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                query: `{
+                    objave {
+                        id
+                        createdAt
+                        naslovObjave
+                        autorObjava {
+                            imeIPrezime
+                        }
+                        fotografija {
+                            handle
+                        }
+                        sazetakObjave
+                        kategorija {
+                            naziv
+                        }
+                    }
+                }`
+            })
+        });
+
+        // store the JSON response when promise resolves
+        const response = await graphcms.json();
+
+        // handle DatoCMS errors
+        if (response.errors) {
+            let errors = response.errors;
+            errors.map((error) => {
+                console.log(error.message);
+            });
+            throw new Error("Aborting: GraphCMS errors");
+        }
+
+        // update blogpost array with the data from the JSON response
+        sveobjave = sveobjave.concat(response.data.objave);
+
+    } catch (error) {
+        throw new Error(error);
+    }
+
+    // format blogposts objects
+    const objaveFormatted = sveobjave.map((item) => {
+        return {
+            id: item.id,
+            date: item.createdAt,
+            author: item.autorObjava.imeIPrezime,
+            title: item.naslovObjave,
+            photo: item.fotografija.handle,
+            excerpt: item.sazetakObjave,
+            category: item.kategorija.naziv
+        };
+    });
+
+    console.log(objaveFormatted);
+
+    // return formatted blogposts
+    return objaveFormatted;
+}
+
+// export for 11ty
+module.exports = getObjave;
