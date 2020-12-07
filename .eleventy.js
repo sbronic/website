f74a6f8e0e24abf8e4269693a419932184f4fefa
Siniša Perović <sinisaperovic@webshaped.biz>
2020-12-07 14:29:38 +0100

    Tekstualni datum i fix broja mjeseca

--- .eleventy.js
+++ .eleventy.js
@@ -27,8 +27,8 @@ module.exports = function (config) {
         var dobj = new Date(dateObj);
         var day = dobj.getDate();
         var year = dobj.getFullYear();
-        //var month = dobj.toLocaleString('hr-HR', { month: 'long' });
-        var month = dobj.getMonth();
+        var month = dobj.toLocaleString('hr-HR', { month: 'long' });
+        //var month = dobj.getMonth()+1;
         return day + "." + month + "." + year + ".";
     });
     // Dan
