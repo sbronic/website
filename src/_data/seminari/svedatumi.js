d90bfc67b11c2761a88bb881c02133c466b2a8d8
Siniša Perović <sinisaperovic@webshaped.biz>
2020-12-11 20:37:24 +0100

    Fixes

--- src/_data/seminari/svedatumi.js
+++ src/_data/seminari/svedatumi.js
@@ -18,7 +18,7 @@ async function getDatumi() {
             },
             body: JSON.stringify({
                 query: `{
-                    datumiSeminara (orderBy: dateAndTime_ASC, where: {dateAndTime_gt: "$today"}) {
+                    datumiSeminara (orderBy: dateAndTime_ASC, stage: PUBLISHED, where: {dateAndTime_gt: "$today"}) {
                         dateAndTime
                         dvorana
                         lokacija {
