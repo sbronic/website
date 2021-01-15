8910b2ae7f04919c5b31dc1f2e26a20849acc4d2
Siniša Perović <sinisaperovic@webshaped.biz>
2021-01-15 10:05:44 +0100

    Fancybox za preview

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,8 @@
 module.exports = {
     title: "Site title",
-    url: "https://hungry-lumiere-36a191.netlify.app"
+    url: "https://hungry-lumiere-36a191.netlify.app",
+    environment: process.env.ELEVENTY_ENV,
+    tracking: {
+        gtag: '12345',
+    }
 };
