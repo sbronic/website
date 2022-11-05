e98d4af9e8a4ff3b1dacac5506ce9d0d01116564
Siniša Perović <sinisaperovic@webshaped.biz>
2022-11-05 16:13:42 +0100

    Novi booking mehanizam + Zoom uključen u datume na početnoj stranici

--- .eleventy.js
+++ .eleventy.js
@@ -1,47 +1,53 @@
-const util = require('util'); // dump filter
-const del = require('del');
+const util = require("util"); // dump filter
+const del = require("del");
 const got = require("got");
 const pluginRSS = require("@11ty/eleventy-plugin-rss");
 const { wordCount } = require("eleventy-plugin-wordcount");
-const moment = require('moment-timezone');
-moment().tz('Europe/Zagreb').format();
+const moment = require("moment-timezone");
+moment().tz("Europe/Zagreb").format();
 //const Canvas = require("canvas");
 //const PDF417 = require("pdf417-generator");
 const htmlmin = require("html-minifier");
 const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 const Image = require("@11ty/eleventy-img");
-const path = require('path');
+const path = require("path");
 //const Cache = require("@11ty/eleventy-cache-assets");
 
 async function imageShortcode(src, alt, sizes) {
-    //let sizes = "(min-width: 400px) 100vw, 50vw"
-    console.log(`Generating image(s) from:  ${src}`)
-    if (alt === undefined) {
-        // Throw an error on missing alt (alt="" works okay)
-        throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
-    }
-    let metadata = await Image(src, {
-        widths: [410, 832],
-        formats: ['webp', 'jpeg'],
-        urlPath: "/img/",
-        outputDir: "./_site/img/",
-        /* =====
+	//let sizes = "(min-width: 400px) 100vw, 50vw"
+	console.log(`Generating image(s) from:  ${src}`);
+	if (alt === undefined) {
+		// Throw an error on missing alt (alt="" works okay)
+		throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+	}
+	let metadata = await Image(src, {
+		widths: [410, 832],
+		formats: ["webp", "jpeg"],
+		urlPath: "/img/",
+		outputDir: "./_site/img/",
+		/* =====
         Now we'll make sure each resulting file's name will
         make sense to you. **This** is why you need
         that `path` statement mentioned earlier.
         ===== */
-        filenameFormat: function (id, src, width, format, options) {
-            const extension = path.extname(src)
-            const name = path.basename(src, extension)
-            return `${name}-${width}w.${format}`
-        }
-    })
-    let lowsrc = metadata.jpeg[0]
-    let highsrc = metadata.jpeg[metadata.jpeg.length - 1]
-    return `<picture>
-    ${Object.values(metadata).map(imageFormat => {
-        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
-    }).join("\n")}
+		filenameFormat: function (id, src, width, format, options) {
+			const extension = path.extname(src);
+			const name = path.basename(src, extension);
+			return `${name}-${width}w.${format}`;
+		},
+	});
+	let lowsrc = metadata.jpeg[0];
+	let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+	return `<picture>
+    ${Object.values(metadata)
+			.map((imageFormat) => {
+				return `  <source type="${
+					imageFormat[0].sourceType
+				}" srcset="${imageFormat
+					.map((entry) => entry.srcset)
+					.join(", ")}" sizes="${sizes}">`;
+			})
+			.join("\n")}
     <img
       src="${lowsrc.url}"
       width="${highsrc.width}"
@@ -49,193 +55,191 @@ async function imageShortcode(src, alt, sizes) {
       alt="${alt}"
       decoding="async"
       class="img-fluid">
-  </picture>`
+  </picture>`;
 }
 
 module.exports = function (config) {
+	let env = process.env.ELEVENTY_ENV;
+	config.addPlugin(UpgradeHelper);
 
-    let env = process.env.ELEVENTY_ENV;
-    config.addPlugin(UpgradeHelper);
+	const dirToClean = "_site/*";
+	del(dirToClean);
 
-    const dirToClean = '_site/*';
-    del(dirToClean);
+	/* IMG plugin shortcode */
+	config.addNunjucksAsyncShortcode("image", imageShortcode);
 
-    /* IMG plugin shortcode */
-    config.addNunjucksAsyncShortcode("image", imageShortcode)
+	/* minify the html output */
+	if (env != "dev") {
+		config.addTransform("htmlmin", function (content, outputPath) {
+			// Eleventy 1.0+: use this.inputPath and this.outputPath instead
+			if (outputPath && outputPath.endsWith(".html")) {
+				let minified = htmlmin.minify(content, {
+					useShortDoctype: true,
+					removeComments: true,
+					collapseWhitespace: true,
+					minifyJS: true,
+					processScripts: "application/ld+json",
+				});
+				return minified;
+			}
 
-    /* minify the html output */
-    if (env != 'dev') {
-        config.addTransform("htmlmin", function (content, outputPath) {
-            // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-            if (outputPath && outputPath.endsWith(".html")) {
-                let minified = htmlmin.minify(content, {
-                    useShortDoctype: true,
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    minifyJS: true,
-                    processScripts: 'application/ld+json'
-                });
-                return minified;
-            }
+			return content;
+		});
+	}
 
-            return content;
-        });
-    }
+	/* Passthrough Copy */
+	config.addPassthroughCopy("src/css");
+	config.addPassthroughCopy("src/fonts");
+	config.addPassthroughCopy("src/images");
+	config.addPassthroughCopy("src/js");
+	config.addPassthroughCopy("src/netlify.toml");
+	config.addPassthroughCopy("src/_redirects");
+	config.addPassthroughCopy("src/sitemap.xml");
 
-    /* Passthrough Copy */
-    config.addPassthroughCopy("src/css");
-    config.addPassthroughCopy("src/fonts");
-    config.addPassthroughCopy("src/images");
-    config.addPassthroughCopy("src/js");
-    config.addPassthroughCopy("src/netlify.toml");
-    config.addPassthroughCopy("src/_redirects");
-    config.addPassthroughCopy("src/sitemap.xml");
+	/* RSS */
+	config.addPlugin(pluginRSS);
+	// RSS file size filter
+	config.addNunjucksAsyncFilter("size", async (url, callback) => {
+		try {
+			const { headers } = await got.head(url);
+			const clength = headers.hasOwnProperty("content-length")
+				? headers["content-length"]
+				: "Unknown";
+			callback(null, clength);
+		} catch (err) {
+			console.error(err);
+			callback(err);
+		}
+	});
+	// RSS file MIME type
+	config.addNunjucksAsyncFilter("type", async (url, callback) => {
+		try {
+			const { headers } = await got.head(url);
+			const clength = headers.hasOwnProperty("content-type")
+				? headers["content-type"]
+				: "Unknown";
+			callback(null, clength);
+		} catch (err) {
+			console.error(err);
+			callback(err);
+		}
+	});
+	// RSS RFC822 time
+	config.addFilter("rfc822", (dateObj) => {
+		return new Date(dateObj).toUTCString();
+	});
 
-    /* RSS */
-    config.addPlugin(pluginRSS);
-    // RSS file size filter
-    config.addNunjucksAsyncFilter("size", async (url, callback) => {
-        try {
-            const { headers } = await got.head(url);
-            const clength = headers.hasOwnProperty("content-length")
-                ? headers["content-length"]
-                : "Unknown";
-            callback(null, clength);
-        } catch (err) {
-            console.error(err);
-            callback(err);
-        }
-    });
-    // RSS file MIME type
-    config.addNunjucksAsyncFilter("type", async (url, callback) => {
-        try {
-            const { headers } = await got.head(url);
-            const clength = headers.hasOwnProperty("content-type")
-                ? headers["content-type"]
-                : "Unknown";
-            callback(null, clength);
-        } catch (err) {
-            console.error(err);
-            callback(err);
-        }
-    });
-    // RSS RFC822 time
-    config.addFilter("rfc822", dateObj => {
-        return new Date(dateObj).toUTCString();
-    });
+	/* Filters */
+	config.addFilter("dump", (obj) => {
+		return util.inspect(obj);
+	});
+	config.addFilter("debugger", (...args) => {
+		console.log(...args);
+		debugger;
+	});
+	// Datum objave
+	config.addFilter("datumObjave", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var day = dobj.getDate();
+		var year = dobj.getFullYear();
+		var month = dobj.toLocaleString("hr-HR", { month: "long" });
+		//var month = dobj.getMonth()+1;
+		return day + ". " + month + " " + year + ".";
+	});
+	// Datum seminara
+	config.addFilter("datumSeminara", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var day = dobj.getDate();
+		var year = dobj.getFullYear();
+		var month = dobj.toLocaleString("hr-HR", { month: "long" });
+		//var month = dobj.getMonth()+1;
+		return day + ". " + month + " " + year + ".";
+	});
+	// Datum seminara broj mjeseca
+	config.addFilter("datumSeminaraNo", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var day = dobj.getDate();
+		var year = dobj.getFullYear();
+		var month = dobj.toLocaleString("hr-HR", { month: "numeric" });
+		//var month = dobj.getMonth()+1;
+		return day + ". " + month + " " + year + ".";
+	});
+	// Dan
+	config.addFilter("datumDan", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var day = dobj.getDate();
+		return day;
+	});
+	// Idući dan
+	config.addFilter("datumIduciDan", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var tomorrow = new Date(dobj);
+		tomorrow.setDate(dobj.getDate() + 1);
+		//var day = dobj.getDate()+1;
+		return tomorrow;
+	});
+	// Mjesec broj
+	config.addFilter("datumMjesecBroj", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var month = dobj.toLocaleString("hr-HR", { month: "numeric" });
+		return month;
+	});
+	// Mjesec
+	config.addFilter("datumMjesec", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var month = dobj.toLocaleString("hr-HR", { month: "short" });
+		return month;
+	});
+	// Mjesec dugi oblik
+	config.addFilter("datumMjesecDugi", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var month = dobj.toLocaleString("hr-HR", { month: "long" });
+		return month;
+	});
+	// Godina
+	config.addFilter("datumGodina", (dateObj) => {
+		var dobj = new Date(dateObj);
+		var year = dobj.getFullYear();
+		return year;
+	});
+	config.addFilter("datumSat", (dateObj) => {
+		//var dobj = new Date(dateObj);
+		var dobj = moment.utc(dateObj).tz("Europe/Amsterdam");
+		//var sat = dobj.getHours();
+		var sat = dobj.hour();
+		var minutes = (dobj.minute() < 10 ? "0" : "") + dobj.minute();
+		var satiminute = sat + ":" + minutes;
+		return satiminute;
+	});
+	// Datum u budućnosti
+	config.addFilter("inFuture", function (dates) {
+		const now = Date.now();
+		const futureDates = dates.filter((date) => {
+			// Filter out dates in the past or falsey values
+			return date && new Date(date).getTime() > now;
+			return futureDates;
+		});
+	});
+	// JSON-LD escape special characters
+	config.addFilter("jsonldesc", (obj) => {
+		const json = JSON.stringify(obj);
+		const jsonesc = JSON.stringify(json);
+		const escstring = JSON.parse(jsonesc);
+		return escstring;
+	});
+	// Limit umjesto slice
+	config.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
 
-    /* Filters */
-    config.addFilter('dump', obj => {
-        return util.inspect(obj);
-    });
-    config.addFilter('debugger', (...args) => {
-        console.log(...args)
-        debugger;
-    });
-    // Datum objave
-    config.addFilter("datumObjave", dateObj => {
-        var dobj = new Date(dateObj);
-        var day = dobj.getDate();
-        var year = dobj.getFullYear();
-        var month = dobj.toLocaleString('hr-HR', { month: 'long' });
-        //var month = dobj.getMonth()+1;
-        return day + ". " + month + " " + year + ".";
-    });
-    // Datum seminara
-    config.addFilter("datumSeminara", dateObj => {
-        var dobj = new Date(dateObj);
-        var day = dobj.getDate();
-        var year = dobj.getFullYear();
-        var month = dobj.toLocaleString('hr-HR', { month: 'long' });
-        //var month = dobj.getMonth()+1;
-        return day + ". " + month + " " + year + ".";
-    });
-    // Datum seminara broj mjeseca
-    config.addFilter("datumSeminaraNo", dateObj => {
-        var dobj = new Date(dateObj);
-        var day = dobj.getDate();
-        var year = dobj.getFullYear();
-        var month = dobj.toLocaleString('hr-HR', { month: 'numeric' });
-        //var month = dobj.getMonth()+1;
-        return day + ". " + month + " " + year + ".";
-    });
-    // Dan
-    config.addFilter("datumDan", dateObj => {
-        var dobj = new Date(dateObj);
-        var day = dobj.getDate();
-        return day;
-    });
-    // Idući dan
-    config.addFilter("datumIduciDan", dateObj => {
-        var dobj = new Date(dateObj);
-        var tomorrow = new Date(dobj);
-        tomorrow.setDate(dobj.getDate() + 1);
-        //var day = dobj.getDate()+1;
-        return tomorrow;
-    });
-    // Mjesec broj
-    config.addFilter("datumMjesecBroj", dateObj => {
-        var dobj = new Date(dateObj);
-        var month = dobj.toLocaleString('hr-HR', { month: 'numeric' });
-        return month;
-    });
-    // Mjesec
-    config.addFilter("datumMjesec", dateObj => {
-        var dobj = new Date(dateObj);
-        var month = dobj.toLocaleString('hr-HR', { month: 'short' });
-        return month;
-    });
-    // Mjesec dugi oblik
-    config.addFilter("datumMjesecDugi", dateObj => {
-        var dobj = new Date(dateObj);
-        var month = dobj.toLocaleString('hr-HR', { month: 'long' });
-        return month;
-    });
-    // Godina
-    config.addFilter("datumGodina", dateObj => {
-        var dobj = new Date(dateObj);
-        var year = dobj.getFullYear();
-        return year;
-    });
-    config.addFilter("datumSat", dateObj => {
-        //var dobj = new Date(dateObj);
-        var dobj = moment.utc(dateObj).tz('Europe/Amsterdam');
-        //var sat = dobj.getHours();
-        var sat = dobj.hour();
-        var minutes = (dobj.minute() < 10 ? '0' : '') + dobj.minute();
-        var satiminute = sat + ':' + minutes;
-        return satiminute;
-    });
-    // Datum u budućnosti
-    config.addFilter("inFuture", function (dates) {
-        const now = Date.now();
-        const futureDates = dates.filter(date => {
-            // Filter out dates in the past or falsey values
-            return date && (new Date(date)).getTime() > now;
-            return futureDates;
-        })
-    });
-    // JSON-LD escape special characters
-    config.addFilter("jsonldesc", obj => {
-        const json = JSON.stringify(obj);
-        const jsonesc = JSON.stringify(json);
-        const escstring = JSON.parse(jsonesc);
-        return escstring;
-    });
-    // Limit umjesto slice
-    config.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
+	/* Shortcodes */
+	config.addShortcode("og_updated_time", () => new Date());
 
-    /* Shortcodes */
-    config.addShortcode("og_updated_time", () => new Date()
-    );
+	/* Plugins */
+	config.addPlugin(wordCount);
 
-    /* Plugins */
-    config.addPlugin(wordCount);
-
-   return {
-        dir: {
-            input: "src",
-            output: "_site"
-        }
-    };
+	return {
+		dir: {
+			input: "src",
+			output: "_site",
+		},
+	};
 };
---
+++ src/_data/homeevents.js
@@ -0,0 +1,205 @@
+// required packages
+const fetch = require("node-fetch");
+const slugify = require("slugify");
+
+// get Seminari
+async function getDatumi() {
+	// Objave array
+	let svidatumi = [];
+	let svizoom = [];
+	const today = new Date().toISOString();
+
+	try {
+		// initiate fetch
+		const graphcms = await fetch(
+			"https://api-eu-central-1.graphcms.com/v2/ckhhcs4a0ds0501xs7lx6803e/master",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+				body: JSON.stringify({
+					variables: { today },
+					query: `query 
+										DatumiSeminara($today: DateTime!) {
+											datumiSeminara (orderBy: dateAndTime_ASC, stage: PUBLISHED, where: {dateAndTime_gt: $today}) {
+													dateAndTime
+													popunjen
+													dvorana
+													lokacija {
+															adresa
+															grad
+															nazivLokacije
+													}
+													odDo
+													seminar {
+															nazivSeminara
+															cijena
+															kategorija {
+																	kod
+																	naziv
+															}
+															fotografija {
+																	handle
+															}
+															sazetakSeminara
+															autoriIPredavaci {
+																	imeIPrezime
+															}
+													}
+											}
+										onLineEdukacije(where: {vrstaEdukacije: Zoom_meeting}, stage: PUBLISHED, orderBy: zoomDatumIVrijemePocetka_ASC) {
+                        naziv
+                        kategorija {
+                            id
+                            kod
+                            naziv
+                        }
+                        trajanje
+                        zoomDatumIVrijemePocetka
+												vrstaEdukacije
+                    }
+									}`,
+				}),
+			}
+		);
+
+		// store the JSON response when promise resolves
+		const response = await graphcms.json();
+
+		// handle Hygraph errors
+		if (response.errors) {
+			let errors = response.errors;
+			errors.map((error) => {
+				console.log(error.message);
+			});
+			throw new Error("Aborting: GraphCMS errors");
+		}
+
+		// update blogpost array with the data from the JSON response
+		svidatumi = svidatumi.concat(response.data.datumiSeminara);
+		svizoom = svizoom.concat(response.data.onLineEdukacije);
+	} catch (error) {
+		throw new Error(error);
+	}
+
+	// konverzija u EUR
+	function konverzija(num) {
+		var m = Number((Math.abs(num) * 100).toPrecision(15)) / 7.5345;
+		return (Math.round(m) / 100) * Math.sign(num);
+	}
+
+	// format seminar objects
+	const formatdatumi = svidatumi
+		.map((item) => {
+			return {
+				datumseminara: item.dateAndTime,
+				popunjen: item.popunjen,
+				lokacijaadresa: item.lokacija.adresa,
+				lokacijagrad: item.lokacija.grad,
+				lokacijanaziv: item.lokacija.nazivLokacije,
+				dvorana: item.dvorana,
+				seminarnaziv: item.seminar.nazivSeminara,
+				seminarnazivslug: slugify(item.seminar.nazivSeminara, {
+					lower: true,
+					strict: true,
+				}),
+				seminarcijena:
+					item.seminar.cijena.toLocaleString("hr-HR") + ",00 Kn + PDV",
+				seminarcijenaEUR:
+					konverzija(item.seminar.cijena).toLocaleString("hr-HR") +
+					" EUR + PDV", // za prikazivanje cijene
+				kategorijanaziv: item.seminar.kategorija.naziv,
+				kategorijaslug: slugify(item.seminar.kategorija.naziv, {
+					lower: true,
+					strict: true,
+				}),
+				kategorijakod: item.seminar.kategorija.kod,
+				photo: item.seminar.fotografija.handle,
+				excerpt: item.seminar.sazetakSeminara,
+				predavaci: item.seminar.autoriIPredavaci,
+				vrijeme: item.odDo,
+			};
+		})
+		.filter(Boolean);
+
+	if (formatdatumi === undefined || formatdatumi.length == 0) {
+		formatdatumi.push("prazno");
+	}
+
+	// format zoom objects
+	const formatedukacije = svizoom
+		.map((item) => {
+			const now = Date.now();
+			const futureDates = item.zoomDatumIVrijemePocetka.filter((date) => {
+				// Filter out dates in the past or falsey values
+				return date && new Date(date).getTime() > now;
+			});
+
+			return {
+				title: item.naziv,
+				titleslug: slugify(item.naziv, { lower: true, strict: true }),
+				category: item.kategorija.naziv,
+				categoryslug: slugify(item.kategorija.naziv, {
+					lower: true,
+					strict: true,
+				}),
+				trajanje: item.trajanje,
+				zoomDatumIVrijemePocetka: futureDates,
+			};
+		})
+		.filter(Boolean);
+
+	var sortzoommeetings = [];
+	var zoommeetings = []
+
+	if (formatedukacije === undefined || formatedukacije.length == 0) {
+		formatedukacije.push("prazno");
+	} else {
+		var zoomlist = {};
+		var zoomitems = [];
+		zoomlist.zoomitems = zoomitems;
+		formatedukacije.forEach((zoom) => {
+			var zoomdates = zoom.zoomDatumIVrijemePocetka;
+			if (zoomdates.length) {
+				zoomdates.forEach((zoomdate) => {
+					var zoomitem = {
+						title: zoom.title,
+						titleslug: zoom.titleslug,
+						category: zoom.category,
+						categoryslug: zoom.categoryslug,
+						zoomDatumIVrijemePocetka: zoomdate,
+						trajanje: zoom.trajanje,
+						datumseminara: zoomdate
+					};
+					zoomlist.zoomitems.push(zoomitem);
+				});
+			}
+		});
+
+		zoommeetings = zoomlist.zoomitems;
+	
+		sortzoommeetings = zoommeetings.sort((a, b) => {
+			return (
+				new Date(a.zoomDatumIVrijemePocetka) -
+				new Date(b.zoomDatumIVrijemePocetka)
+			); // descending
+		});
+	}
+
+	const combined = sortzoommeetings.concat(formatdatumi)
+	
+	const sortcombined = combined.sort((a, b) => {
+		return (
+			new Date(a.datumseminara) -
+			new Date(b.datumseminara)
+		); // descending
+	});
+	console.log(sortcombined)
+	// return formatted blogposts
+	return sortcombined;
+}
+
+// export for 11ty
+module.exports = getDatumi;
